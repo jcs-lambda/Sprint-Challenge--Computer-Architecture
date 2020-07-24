95757755f8f6109ce028ee86fc74f432baf120ed
jcs-lambda <57103874+jcs-lambda@users.noreply.github.com>
2020-07-24 10:20:01 -0500

    interrupts and keyboard

--- cpu.py
+++ cpu.py
@@ -437,6 +437,8 @@ if __name__ == '__main__':
     cur_dir = dirname(realpath(__file__))
     files = [
         join(cur_dir, 'sctest.ls8'),
+        join(cur_dir, 'keyboard.ls8'),
+        join(cur_dir, 'interrupts.ls8'),
     ]
     c = CPU()
     for f in files:
